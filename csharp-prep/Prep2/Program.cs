707dc794a42c832a95c83f4430d7dc9daf10b981
Jospine <nzi19001@byui.edu>
2024-11-01 22:23:35 +0300

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter marks (0-100): ");
+        int marks = int.Parse(Console.ReadLine());
+        char grade;
+
+        if (marks >= 90)
+        {
+            grade = 'A';
+        }
+        else if (marks >= 80)
+        {
+            grade = 'B';
+        }
+        else if (marks >= 70)
+        {
+            grade = 'C';
+        }
+        else if (marks >= 60)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'E';
+        }
+
+        Console.WriteLine($"The letter grade is: {grade}");
+        if (marks >= 70) 
+        {
+            Console.WriteLine($"Congratulations passed Exam!");
+        }
+        else
+        {
+            Console.WriteLine($"failed try again please");
+        }
     }
 }
