5ed753dd306b9e1ff6c3cddb949dd39a5fde199f
Jospine <nzi19001@byui.edu>
2024-11-02 22:46:15 +0300

    finished prep 3 program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random(); 
+        string playAgain;
+
+        do
+        {
+            int magicNumber = random.Next(1, 101); 
+            int guess; 
+            int guessCount = 0; 
+            
+
+            Console.WriteLine($"The magic number is {magicNumber}."); 
+            Console.WriteLine("I have picked a number between 1 and 100. Try to guess it!");
+
+            
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+
+                while (!int.TryParse(Console.ReadLine(), out guess) || guess < 1 || guess > 100)
+                {
+                    Console.Write("Please enter a valid number between 1 and 100: ");
+                }
+
+                guessCount++;
+                
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower"); 
+                
+                }
+                else 
+                {
+                    Console.WriteLine($"You guessed it! The magic number was {magicNumber}. It took you {guessCount} guesses.");
+                    break; 
+                }
+            }
+
+        
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes"); 
+
+        Console.WriteLine("Thanks for playing! See you soon!");
     }
 }
