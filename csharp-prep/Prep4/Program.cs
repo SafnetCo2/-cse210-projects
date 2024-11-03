eb6f0d149fe7168a7b885c6a3723633ef8bcf290
Jospine <nzi19001@byui.edu>
2024-11-03 20:31:36 +0300

    finished core prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List <int> numbers = new List<int>();
+        int number;
+        Console.WriteLine("Enter a list of numbers: ");
+        do
+        {
+            Console.Write("Enter a number");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+        int sum = numbers.Sum();
+        Console.WriteLine($"sum is {sum}");
+        //average
+        double average = numbers.Average();
+        Console.WriteLine($"the average: {average}");
+
+        //maximum number
+        int max = numbers.Max();
+        Console.WriteLine($"maximum number is: {max}");
+        
     }
 }
