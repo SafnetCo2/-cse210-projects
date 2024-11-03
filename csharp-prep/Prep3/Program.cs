17ed1cd082ec8793543c48c82ec9708e4bfca9a8
Jospine <nzi19001@byui.edu>
2024-11-03 20:05:14 +0300

    modiy  play game program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random(); 
+        Random random = new Random();
         string playAgain;
 
         do
         {
-            int magicNumber = random.Next(1, 101); 
-            int guess; 
-            int guessCount = 0; 
-            
+            int magicNumber = random.Next(1, 101);
+            int guess;
+            int guessCount = 0;
 
-            Console.WriteLine($"The magic number is {magicNumber}."); 
-            Console.WriteLine("I have picked a number between 1 and 100. Try to guess it!");
 
-            
+            Console.WriteLine($"magic number is {magicNumber}.");
+            Console.WriteLine("number is between 1 and 100. guess please!");
+
+
             while (true)
             {
-                Console.Write("What is your guess? ");
+                Console.Write(" your guess? ");
 
                 while (!int.TryParse(Console.ReadLine(), out guess) || guess < 1 || guess > 100)
                 {
-                    Console.Write("Please enter a valid number between 1 and 100: ");
+                    Console.Write("Enter a valid number between 1 and 100: ");
                 }
 
                 guessCount++;
-                
+
 
                 if (guess < magicNumber)
                 {
@@ -37,22 +37,22 @@ class Program
                 }
                 else if (guess > magicNumber)
                 {
-                    Console.WriteLine("Lower"); 
-                
+                    Console.WriteLine("Lower");
+
                 }
-                else 
+                else
                 {
-                    Console.WriteLine($"You guessed it! The magic number was {magicNumber}. It took you {guessCount} guesses.");
-                    break; 
+                    Console.WriteLine($"Correct guessed it! The magic number was {magicNumber}. It took you {guessCount} guesses.");
+                    break;
                 }
             }
 
-        
-            Console.Write("Do you want to play again? (yes/no): ");
+
+            Console.Write("play again? (yes/no): ");
             playAgain = Console.ReadLine().ToLower();
 
-        } while (playAgain == "yes"); 
+        } while (playAgain == "yes");
 
-        Console.WriteLine("Thanks for playing! See you soon!");
+        Console.WriteLine(" See you soon!");
     }
 }
