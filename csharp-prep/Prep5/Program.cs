7e308e3cb77b8c345937718e21a1aff2d5420f77
Jospine <nzi19001@byui.edu>
2024-11-04 13:29:20 +0300

    Fininshed prep05

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Globalization;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        Console.WriteLine($"Hello,{userName}!");
+
+        int favoriteNumber = PromptUserNumber();
+        Console.WriteLine($"my favorite number is {favoriteNumber}");
+
+        int squaredNumber = SquaredNumber(favoriteNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+//function to display Welcome
+static void DisplayWelcome()
+    {
+        Console.WriteLine("Hello Welcome to the Program");
+    }
+    //prompt username functions
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+
+
+    }
+    static int PromptUserNumber()
+        {
+        Console.Write("Enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+
+            
+        }
+    //square function
+    static int SquaredNumber(int number)
+    {
+        return number* number;
+    }
+    
+    static void DisplayResult(string userName, int squaredNumber)
+    {
+        Console.WriteLine($"{userName} if  squared the number is {squaredNumber}");
     }
 }
+
+    
+
