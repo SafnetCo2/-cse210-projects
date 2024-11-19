29b30b8a9b27e2af89d16ed7e4625f9711eb6485
Jospine <nzi19001@byui.edu>
2024-11-19 14:38:56 +0300

    finished assignement learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -12,21 +12,22 @@ namespace FractionSample
             Fraction frac3 = new Fraction(6, 7);   
 
             // Output the fraction and its decimal representation
+            //1/1 =1
             Console.WriteLine(frac1.GetFractionString());  
             Console.WriteLine(frac1.GetDecimalValue());   
-
+            //6/1=6
             Console.WriteLine(frac2.GetFractionString());  
             Console.WriteLine(frac2.GetDecimalValue());   
-
+            //6/7
             Console.WriteLine(frac3.GetFractionString());  
             Console.WriteLine(frac3.GetDecimalValue());   // 0.8571428571428571
 
             // Modifying values using setters
             frac1.Top = 3;
             frac1.Bottom = 4;
-
-            Console.WriteLine(frac1.GetFractionString());  // 3/4
-            Console.WriteLine(frac1.GetDecimalValue());   // 0.75
+            //3/4
+            Console.WriteLine(frac1.GetFractionString());  
+            Console.WriteLine(frac1.GetDecimalValue());   
         }
     }
 }
