2a3cb516698993728369f7bf96a96532e5235930
Jospine <nzi19001@byui.edu>
2024-12-13 22:38:03 +0300

    removed conflict folder of polymorphism types

--- csharp-prep/Prep5/obj/Debug/net8.0/Prep5.AssemblyInfo.cs
+++ csharp-prep/Prep5/obj/Debug/net8.0/Prep5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prep5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc966e409c95e3815d3978b359aa874439f75f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f628b0ade7eedb51fdf25c0010ed624a0fe96c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prep5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prep5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- prepare/Learning02/obj/Debug/net8.0/Learning02.AssemblyInfo.cs
+++ prepare/Learning02/obj/Debug/net8.0/Learning02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc966e409c95e3815d3978b359aa874439f75f9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f628b0ade7eedb51fdf25c0010ed624a0fe96c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
